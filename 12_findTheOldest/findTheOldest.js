6669bdc16676ec7fae1e548d0466241a12511c0f
vivizuma <vitomanz@hotmail.co.uk>
2022-10-22 18:39:41 +0100

    completed up to 13

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,19 +1,16 @@
 const findTheOldest = function (people) {
-  //find the oldest
-  //.sort()
-  const arr = people.map(())
-  const yearNow = new Date().getFullYear();
-  if (people.hasOwnProperty.yearOfDeath === false) {
-    people.yearOfDeath.push(yearNow);
+  //loop through objects and add year of death as current year if not present
+  for (i = 0; i < people.length; i++) {
+    if (!people[i].yearOfDeath) {
+      people[i].yearOfDeath = new Date().getFullYear();
+    }
   }
+  people.sort(
+    (a, b) => (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth)
+  );
+  console.log(people[0]);
+  return people[0];
 
-  console.log(yearNow);
-  const oldest = people.sort(function (a, b) {
-    const person1 = a.yearOfDeath - a.yearOfBirth;
-    const person2 = b.yearOfDeath - a.yearOfBirth;
-    return person1 > person2 ? -1 : 1;
-  });
-  console.log(oldest);
 };
 
 const people = [
@@ -21,18 +18,22 @@ const people = [
     name: "Carly",
     yearOfBirth: 1942,
     yearOfDeath: 1970,
+    yearsLived: 1970 - 1942,
   },
   {
     name: "Ray",
     yearOfBirth: 1962,
     yearOfDeath: 2011,
+    yearsLives: 2011 - 1962,
   },
   {
     name: "Jane",
     yearOfBirth: 1912,
     yearOfDeath: 1941,
+    yearsLived: 1941 - 1912,
   },
-  { name: "dog", yearOfBirth: 1990 },
+  { name: "dog", yearOfBirth: 1990, yearsLived: 2022 - 1990 },
+  { name: "cat", yearOfBirth: 2007, yearsLived: 2022 - 2007 },
 ];
 
 findTheOldest(people);
